56dc0265d2585b72f664145aaf49d3571006ee16
Saahir <103967382+saahirvazirani@users.noreply.github.com>
2024-08-03 18:08:12 -0400

    spline fix

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,35 +12,33 @@ const Homepage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      <div className="h-full flex flex-col-reverse lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         {/* IMAGE CONTAINER */}
         <div className="h-1/4 sm:h-1/3 md:h-1/2 lg:h-full lg:w-1/2 relative rounded-lg">
-  <Spline scene="https://prod.spline.design/HdEDOpqlP7cQHIvD/scene.splinecode" />
-</div>
+          <Spline scene="https://prod.spline.design/HdEDOpqlP7cQHIvD/scene.splinecode" />
+        </div>
         {/* TEXT CONTAINER */}
-        <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
+        <div className="h-3/4 sm:h-2/3 md:h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
           {/* TITLE */}
           <h1 className="text-4xl md:text-6xl font-bold">
             Youth Advocate, Policy and Technology Enthusiast, Passionate Rower
           </h1>
           {/* DESC */}
           <p className="md:text-xl">
-          Welcome to my personal website, a fusion of innovation and creativity. With a sharp eye for aesthetics and a mastery of code, this collection highlights my diverse projects, each reflecting my unwavering commitment to excellence.
+            Welcome to my personal website, a fusion of innovation and creativity. With a sharp eye for aesthetics and a mastery of code, this collection highlights my diverse projects, each reflecting my unwavering commitment to excellence.
           </p>
           {/* BUTTONS */}
           <div className="w-full flex gap-4">
-          <Link
-          href="/portfolio">
-            <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white hover:transform hover:scale-105 transition duration-300">
-              View My Work
-            </button>
-  </Link>
-  <Link
-          href="/contact">
-            <button className="p-4 rounded-lg ring-1 ring-black hover:transform hover:scale-105 transition duration-300">
-              Contact Me
-            </button>
-          </Link>
+            <Link href="/portfolio">
+              <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white hover:transform hover:scale-105 transition duration-300">
+                View My Work
+              </button>
+            </Link>
+            <Link href="/contact">
+              <button className="p-4 rounded-lg ring-1 ring-black hover:transform hover:scale-105 transition duration-300">
+                Contact Me
+              </button>
+            </Link>
           </div>
         </div>
       </div>
